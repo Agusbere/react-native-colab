9183d17cb66981e1faf9046f6d3cb5639d00b4f1
Android Studio <devandroid@ort>
2025-08-18 12:05:49 -0300

    arreglo de App.js xd

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hola soy klawo!</Text>
+      <Text>Ahora soy gusa!</Text>
       <StatusBar style="auto" />
     </View>
   );
